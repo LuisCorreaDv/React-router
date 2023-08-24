13ebdfd4008d9e933f7e7e2cb39c56a4fda255ef
LuisCorreaDv <dev.luis.correa@gmail.com>
2023-08-23 18:55:20 -0600

    Conexion con API con la libreria axios

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+//Libreria para hacer peticiones HTTP
+
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "http:localhost:5000"
+})
+
+export const buscar = async (url, setData) => {
+    const respuesta = await api.get(url)
+    //data es una llave donde axios guarda los datos de la url
+    setData(respuesta.data)
+}
